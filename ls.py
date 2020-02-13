458cf75c0fa0c43b5f7014f7830a14c9c9687a7f
Shenglin Sun <sunshenglin12@gmail.com>
2020-02-13 12:42:49 -0600

    Update ls.py

--- ls.py
+++ ls.py
@@ -4,7 +4,7 @@ path = os.getcwd() # use .getcwd to get the path of current location
 
 def getname(path):
     if os.path.isdir(path): 
-        filder = os.listdir(path) + ["    "] # use .listdir to list filder's name of current address
+        filder = os.listdir(path) # use .listdir to list filder's name of current address
         return filder
 
     else:
