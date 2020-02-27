0927bcc3c57705c334cba5e08ea2e7b47d7ede21
Shenglin Sun <sunshenglin12@gmail.com>
2020-02-27 14:02:57 -0600

    Add files via upload

--- lsh.py
+++ lsh.py
@@ -4,6 +4,6 @@ import Input, os
 #
 def ls(**kwargs):
     for items in os.listdir(os.getcwd()):
-        size=os.path.getsize(items)    
-        print (items)#type of paramater, doesn't work if print(items+size)
-        print (size)
+        size=os.path.getsize(items)
+        print (items + "    " + str(size) + "mb" )#type of paramater, doesn't work if print(items+size)
+        #print (size)
